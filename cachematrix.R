012f185ff027436281820792220e47140dbc01f1
Jay W Johnson <tastethejava@hotmail.com>
2014-07-21 08:26:23 +0800

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -11,13 +11,19 @@
 makeCacheMatrix <- function(x = matrix()) {
   
   i = NULL
+  ## Function for setting the stored matrix.
   set = function(y){
     x <<- y
+    ## Reset the cached inverse.
     i <<- NULL
   }
+  ## Get the matrix 'x'
   get = function() x
+  ## Store the inverse in this scope.
   setinverse = function(inverse) i <<- inverse
+  ## Retrieve the cached inverse from this scope.
   getinverse = function() i
+  ## Return a list of functions for accessing this scope.
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -31,13 +37,19 @@ cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   ## Cache the inverse within the object 'x'
   
+  ## Get cached inverse if it exists.
   i = x$getinverse()
+  ## If inverse already exists then return it and exit.
   if(!is.null(i)){
     message("getting cached data")
     return(i)
   }
+  
+  ## Compute inverse of 'x'
   data = x$get()
   i = solve(data, ...)
+  ## Cache the inverse in the 'x' object.
   x$setinverse(i)
+  ## Return the inverse.
   i
 }
